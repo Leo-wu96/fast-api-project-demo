383c81e23b873839433b429f6fa97a722564ad9b
zixian.wu <zixian.wu@shopee.com>
2024-05-09 10:17:07 +0800

    test

---
+++ test/test_example.py
@@ -0,0 +1,24 @@
+'''
+Author: zixian.wu@shopee.com
+Date: 2024-05-08 15:27:51
+LastEditTime: 2024-05-08 15:57:21
+Description: file content
+'''
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+
+app = FastAPI()
+
+@app.get("/")
+async def read_main():
+    return {"msg": "hello world"}
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+    
+
