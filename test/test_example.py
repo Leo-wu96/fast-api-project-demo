51566a5d769d7ba241f77cce2a9a0511c90df9f3
zixian.wu <zixian.wu@shopee.com>
2024-05-10 18:25:45 +0800

    test example 0510

--- test/test_example.py
+++ test/test_example.py
@@ -1,7 +1,7 @@
 '''
 Author: zixian.wu@shopee.com
 Date: 2024-05-08 15:27:51
-LastEditTime: 2024-05-08 15:57:21
+LastEditTime: 2024-05-10 10:42:05
 Description: file content
 '''
 from fastapi import FastAPI
@@ -20,5 +20,3 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
-    
-
